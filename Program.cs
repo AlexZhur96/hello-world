4efaeb77c081c3c8e2847b2947d1ddd4fbd55494
AlexZhur96 <94372240+AlexZhur96@users.noreply.github.com>
2021-11-15 16:18:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; // Добавил static System.Console, чтобы не писать Console в коде
+ 
+namespace HelloWorldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Привет, мир!"); //WriteLine(" "); вывод текст в консоль
+
+            ReadKey();                //ReadKey(); задерживает консоль на экране
+        }
+    }
+}
